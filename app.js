90d8805101fd8b7bb312dbbee3dc76d03e98e602
MsNiz <mniz416@gmail.com>
2021-06-08 14:21:13 -0400

    end of leson 1

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-var message = 'Hello Node!';
+const profileDataArgs = process.argv.slice(2);
 
-var sum = 5 + 3;
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
 
-console.log(message);
-console.log(sum);
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
