30e64c1a498399ecae52d7ccb021f5a3081ad9c8
MsNiz <mniz416@gmail.com>
2021-06-09 14:08:15 -0400

    inquire whats ur name

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-console.log(profileDataArgs);
 
-const [name, github] = profileDataArgs;
 
-console.log(name, github);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const pageHTML = generatePage(name, github);
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
